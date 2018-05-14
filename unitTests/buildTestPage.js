0139185f36ece654a3027e9667fcc991b127d566
Shane Connon <shane.connon@gmail.com>
2018-05-14 18:20:59 +0100

    unit test framework updates

--- unitTests/buildTestPage.js
+++ unitTests/buildTestPage.js
@@ -6,6 +6,10 @@ function mochaUrl (mochaFile) {
     return path.resolve(__dirname, "../node_modules/mocha/", mochaFile);
 }
 
+function chaiUrl (mochaFile) {
+    return path.resolve(__dirname, "../node_modules/chai/", mochaFile);
+}
+
 var files = [];    
 var unitTests = path.resolve(__dirname, "../testBin/unitTests");
 if (fs.existsSync(unitTests)) {
@@ -19,18 +23,22 @@ files = files.map(x => x.fullname.replace(/\\/g, "/").replace(/^.+?testBin\//i, 
 
 var page = `<html>
   <head>
-    <title>Tests</title>
+    <title>Material time tests</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="${mochaUrl("mocha.css")}" />
   </head>
   <body>
-    <script src="${path.resolve(__dirname, "../node_modules/requirejs/require.js")}"></script>
     <script src="${mochaUrl("mocha.js")}"></script>
+    <script src="${chaiUrl("chai.js")}"></script>
+    <script src="${path.resolve(__dirname, "../node_modules/requirejs/require.js")}"></script>
+
+    <div>Test page generated using ${__filename}</div>
 
     <div id="mocha"></div>
     
     <script>
+      chai.should();
       mocha.setup("bdd");
 
       require([
