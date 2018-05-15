0bdf9e6f951754587d919c6f1594dfc3ee3f13f4
Shane Connon <shane.connon@gmail.com>
2018-05-15 17:04:54 +0100

    am/pm tests

---
+++ unitTests/12hr_test.ts
@@ -0,0 +1,106 @@
+import { getHours, getMinutes } from "../src/time";
+import { AmPm } from "../src/distance";
+import { PublicClock, publicClock } from "../src/publicClock";
+import { DiContext } from "../src/di";
+import { Clock } from "../src/clock";
+
+describe("12hr_Tests.ts", () => {
+
+    var clock: PublicClock, ctxt: DiContext
+    beforeEach(() => {
+        clock = publicClock(ctxt = new DiContext({
+            time: {
+                hour: 0,
+                minute: 0
+            },
+            closeOnSelect: true,
+            mode: 12
+        }));
+
+        document.body.appendChild(clock.element);
+    });
+    
+    afterEach(() => {
+        clock.dispose();
+    });
+
+    it("sets time correctly in 12hr mode, 1", function() {
+
+        // arrange
+        // act
+        clock.setTime(1, 20);
+
+        // assert
+        clock.getTime().hour.should.be.eql(1);
+        clock.getTime().minute.should.be.eql(20);
+    });
+
+    it("sets time correctly in 12hr mode, 2", function() {
+
+        // arrange
+        // act
+        clock.setTime(13, 20);
+
+        // assert
+        clock.getTime().hour.should.be.eql(13);
+        clock.getTime().minute.should.be.eql(20);
+    });
+
+    it("shows user time in 12hr mode", function() {
+
+        // arrange
+        // act
+        clock.setTime(13, 20);
+
+        // assert
+        ctxt.getInnerElement<HTMLInputElement>(".mtl-hour").value.should.be.eql("1");
+    });
+
+    it("should highlight pm when pm time set", function() {
+
+        // arrange
+        // act
+        clock.setTime(13, 20);
+
+        // assert
+        var ok = false;
+        for (var i = 0; i < ctxt.buildHoursElements().pm.attributes.length; i++) {
+            if (ctxt.buildHoursElements().pm.attributes[i].name === "aria-pressed") {
+                ok = true;
+                return;
+            }
+        }
+
+        if (!ok) throw new Error("FAIL 1");
+        
+        for (var i = 0; i < ctxt.buildHoursElements().am.attributes.length; i++) {
+            if (ctxt.buildHoursElements().am.attributes[i].name === "aria-pressed") {
+                throw new Error("FAIL 2")
+            }
+        }
+    });
+
+    it("should highlight am when am time set", function() {
+
+        // arrange
+        // act
+        clock.setTime(1, 20);
+
+        // assert
+        var ok = false;
+        for (var i = 0; i < ctxt.buildHoursElements().am.attributes.length; i++) {
+            if (ctxt.buildHoursElements().am.attributes[i].name === "aria-pressed") {
+                ok = true;
+                return;
+            }
+        }
+
+        if (!ok) throw new Error("FAIL 1");
+        
+        for (var i = 0; i < ctxt.buildHoursElements().pm.attributes.length; i++) {
+            if (ctxt.buildHoursElements().pm.attributes[i].name === "aria-pressed") {
+                throw new Error("FAIL 2")
+            }
+        }
+    });
+});
